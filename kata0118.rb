4a454a738d68f963f6388a8a2f8243bcfc3332bd
doverh <doverheld@gmail.com>
2017-01-18 13:40:14 -0500

    updating class test

---
+++ kata0118.rb
@@ -0,0 +1,22 @@
+
+def minedmindskata(num)
+
+  if (num >=  1 && num <= 100)
+
+	if num % 3 == 0 && num % 5 == 0
+		"Mined Minds"
+
+	elsif num % 3 == 0
+		"Mined"
+
+	elsif num % 5 == 0
+		"Minds"
+	
+	else
+	    num
+
+	end
+  
+  end	
+
+end
--- test_kata0118.rb
+++ test_kata0118.rb
@@ -4,9 +4,34 @@
 	require "minitest/autorun"
 	require_relative "kata0118.rb" # use relative because is in the same folder
 
-	#Declare the class and define that it is a class test
+	#Declare the class and define that it is a class test using Minitest::Test
 	class TestMinedMindsKata < Minitest::Test
 			def test_1_returns_1
 				assert_equal(1,1)
+			end
+
+			def test_function_returns_1
+				assert_equal(1, minedmindskata(1))
+				#assert_equal(2, minedmindskata(1))
+			end
+
+			def test_function_returns_2
+				assert_equal(2,minedmindskata(2))
+			end
+
+			def test_function_return_3
+				assert_equal("Mined", minedmindskata(3))
+			end
+
+			def test_function_return_5
+				assert_equal("Minds", minedmindskata(5))
+			end
+
+			def test_funtion_return_15
+				assert_equal("Mined Minds", minedmindskata(15))
+			end
+
+			def test_funtion_return_101
+				assert_nil(nil, minedmindskata(101))
 			end
 	end
