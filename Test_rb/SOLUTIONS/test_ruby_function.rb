7db95bd4be09ad2b72009efc72ed892bf487420c
doverh <doverheld@gmail.com>
2017-01-25 12:16:14 -0500

    Updating exercise functions

--- Test_rb/SOLUTIONS/test_ruby_function.rb
+++ Test_rb/SOLUTIONS/test_ruby_function.rb
@@ -4,6 +4,12 @@ require_relative "ruby_function.rb"
 class TestRubyFunction < Minitest::Test
 
 	#test 2 number return result
+	def test_if_is_a_number
+		assert_equals('Hello Folks',add('Hello',' Folks'))
+
+   	end     
+    
+
 	def test_1_plus_1_returns_2
 		assert_equal(2,add(1,1))
 	end
@@ -15,11 +21,19 @@ class TestRubyFunction < Minitest::Test
      def test_float_numbers_addition
      	assert_equal(10.88, add(5.44,5.44))
      end	
+    
+
+    #Another way to return check if is a number 
+   	def test_if_is_a_number
+		assert_nil(nil,add('a',1))
+
+   	end
+
+   	#Check if strings can be added
+	 # def test_if_0_is_passed_as_argument
+     # 	assert_equal()
      
-     def test_if_2_numbers_where_inserted
-     
-     end
-     
+
 		# def test_if_parameter_is_number
 	# 	assert_equal()
 end
--- Test_rb/ruby_function.rb
+++ Test_rb/ruby_function.rb
@@ -1,11 +1,19 @@
 
 
 def add(num1, num2)
-	if ((num1.is_a? Numeric) && (num2.is_a? Numeric))  
+	#Test if variables are strings, add and return them
+	if ((num1.is_a? String) && (num2.is_a? String))  
 		puts num1 + num2
 		return num1 + num2
+
+	#Test if variables are numbers, add and return them
+	elsif ((num1.is_a? Numeric) && (num2.is_a? Numeric))  
+		puts num1 + num2
+		return num1 + num2
+
+		
 	else
-		print "Either num1:#{num1} and num2:#{num2} should be numbers!"
+		print "Either #{num1} and #{num2} should be numbers!"
 	end
 
 end
