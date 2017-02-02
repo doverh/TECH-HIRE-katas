137abf5f50665c89bc21a253c5c57331108948bf
doverh <doverheld@gmail.com>
2017-02-01 22:29:29 -0500

    Updating isbn10 files

--- isbn10.rb
+++ isbn10.rb
@@ -1,25 +1,82 @@
 
+#Build my string
+#return this string
+#check X
+#check 10 or 13
 
-def prepare_isbn(isbn)
-	my_isbn = isbn.delete(' -')
-	check = []
-	puts my_isbn	
-	# isbn.each do |num|
-	 	for i in (0..my_isbn.length - 1)
-			if my_isbn[i] == "0"
-		 		check.push(my_isbn[i])
-		 	elsif !(my_isbn[i].to_i>= 1)
+def check_is_number(isbn)
+    
+	isbn.each do |isbn_n|
+    	isbn_number = isbn_n.delete(' -')
+
+    #check the chars, unless the last
+    	for i in (0..isbn_number.to_s.length - 2)
+			#Check if char is zero
+			if isbn_number[i] == "0"
+		 	#if not zero, check if is not a number
+		 	elsif !(isbn_number[i].to_i>= 1)
 		 	 	print "Break loop #{isbn[i]}"
 		 	 	return false
-		 	else
-		 		check.push(my_isbn[i])
-		 	end
+			end
 		end
-		print check
-		return true
-	#end
+	end
+	return true
 end
 
-prepare_isbn(" 6-5839&0")
+def check_last_char_x(isbn)
+
+	isbn.each do |isbn_n|
+    	isbn_number = isbn_n.delete(' -')
+
+		if isbn_number[-1] == 'x' or isbn_number[-1] == 'X'
+		
+		elsif isbn_number[-1] == "0"
+		
+		#return false if last char is not X, 0, or Number
+		elsif !(isbn_number[-1].to_i>= 1)
+		 	print "Break loop #{isbn[-1]}"
+			return false	
+		else
+		
+		end
+	end
+	return true
+end
+	
+#Function to check if number has length allowed to ISBN
+def check_lenght(isbn)	
+	isbn.each do |isbn_n|
+		isbn_number = isbn_n.delete(' -')
+			 if isbn_number.length == 10 or isbn_number.length ==13	
+				return true
+			else
+				return false
+			end
+	end
+end
+
+# def prepare_isbn(isbn)
+# 	my_isbn = isbn.delete(' -')
+# 	check = ""
+# 	puts my_isbn	
+# 	# isbn.each do |num|
+# 	 	for i in (0..my_isbn.length - 1)
+# 			if my_isbn[i] == "0"
+# 		 		check << my_isbn[i]
+# 		 	elsif !(my_isbn[i].to_i>= 1)
+# 		 	 	print "Break loop #{isbn[i]}"
+# 		 	 	return false
+# 		 	else
+# 		 		#String instead of array 
+# 		 		check << my_isbn[i]
+# 		 	end
+# 		end
+# 		print check
+# 		#return the number
+# 		return true
+# 	#end
+# end
+
+# prepare_isbn(" 6-5839&0")
 
 
--- test_isbn10.rb
+++ test_isbn10.rb
@@ -10,18 +10,39 @@ require_relative "isbn10.rb"
 
 class TestIsbn < Minitest::Test
 
-def test_isbn_accept_spaces
-assert_equal(true,prepare_isbn("978 0 471 48648 0"))
+def test_isbn_has_10_chars
+	assert_equal(true,check_is_number(["978 5 471 486"]))
+end
+
+def test_isbn_has_10_more_then_chars
+	assert_equal(false,check_lenght(["978 0 471 48648 02355"]))
+end
+
+# def test_isbn_special_char
+# 	assert_equal(false,check_is_isbn("970()471-48"))
+# end
+
+def test_isbn_with_last_X
+ 	assert_equal(true,check_last_char_x(["978 5 471 48X"]))
 end
 
 
- def test_isbn_accept_hiffen
- assert_equal(true,prepare_isbn("978-0-471-48648-0"))
- end
+def test_isbn_with_last_not_X
+ 	assert_equal(false,check_last_char_x(["978 5 471 48C"]))
+end
 
-def test_isbn_special_char
- assert_equal(false,prepare_isbn("970()471-48648-0"))
- end
+# def test_isbn_accept_spaces
+# assert_equal(true,prepare_isbn("978 0 471 48648 0"))
+# end
+
+
+#  def test_isbn_accept_hiffen
+#  assert_equal(true,prepare_isbn("978-0-471-48648-0"))
+#  end
+
+# def test_isbn_special_char
+#  assert_equal(false,prepare_isbn("970()471-48648-0"))
+#  end
 
 
 end
