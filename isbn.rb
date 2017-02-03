128a5d42901273a95123517a82822b381c8833fb
doverh <doverheld@gmail.com>
2017-02-02 22:56:01 -0500

    Refactoring isbn13

--- isbn.rb
+++ isbn.rb
@@ -3,7 +3,7 @@ def check_number_isbn10(isbn)
 
 	isbn.each do |isbn_n|
     	#Delete spaces and hifens
-    	number = isbn_n.delete(' -')
+    	number = isbn_n.delete(' -') #Alternative could be .gsub(/[-\s+]/, "") 
     	#Separate last digit to check if is X or x
     		isbn_number = number[0..8]
     		isbn_digit = number[9]
@@ -46,27 +46,51 @@ def check_length(isbn)
 	return true
 end
 
-# def check_isbn_10(isbn)
+def check_sum_10(isbn)
 
-# 	isbn.each do |isbn_number|
-# 		total = 0
-# 		for i in (0..isbn_number.to_s.length - 2)
-# 			#puts isbn_number[i].to_i
-# 			#puts "isbn_number[i] = #{isbn_number[i].to_i * (i+1)}"
-# 			total += isbn_number[i].to_i * (i+1)
-# 		end
-# 		puts "Total = #{total%11}"
-# 		#puts "last = #{isbn_number[-1]}"
-# 		#total = total % 11 
-# 		check_number = isbn_number[-1]
-# 		if (check_number=='x' || check_number=='X') && (total % 11 == 10)
-# 			break
-# 		elsif  (total % 11 != isbn_number[-1].to_i)
-# 			return false 	
-# 		end
-# 	end	
-# 	return true
-# end
+	isbn.each do |isbn_number|
+		total = 0
+		position = 1
+		isbn_number[0..8].each_char do |calc_num|
+			total += calc_num.to_i * position
+			position +=1
+		end
+		total = total % 11
+		if ((isbn_number[9] == 'X' || isbn_number[9] == 'x')  && total == 10)
+			#Go to next ISBN on array
+		elsif (isbn_number[9].to_i == total)
+			#Go to next ISBN on array
+		else
+			return false
+		end
+	end	
+	return true
+end
+
+
+
+def check_sum_13(isbn)
+
+	isbn.each do |isbn_number|
+		total = 0
+		position = 1
+		isbn_number[0..11].each_char do |calc_num|
+			position.even? ? total += calc_num.to_i * 3 : total += calc_num.to_i * 1
+			position +=1
+		end
+
+		puts "Total = #{total%11}"
+		puts "last = #{isbn_number[9]}"
+		total = (10 - total % 10) % 10
+		if (total == isbn_number[12].to_i)
+			#Go to next ISBN on array			
+		else 
+			return false
+		end	
+	end
+	return true
+end
+
 
 # def check_isbn_13(isbn)
 
--- test_isbn.rb
+++ test_isbn.rb
@@ -11,6 +11,8 @@ require_relative "isbn.rb"
 class TestIsbn < Minitest::Test
 
 
+#ISBN10 TESTS
+
 def test_isbn_lenght_10_chars
 	assert_equal(true,check_length(["978 5 471 486"]))
 end
@@ -18,15 +20,6 @@ end
 def test_isbn_incorret_number_of_chars
 	assert_equal(false,check_length(["978 471 486"]))
 end
-
-def test_isbn_13_length
-	assert_equal(true,check_length(["978 5 471 486-425"]))
-end
-
-def test_isbn_13_incorret_number_of_chars
-	assert_equal(false,check_length(["978 471 486-1-1-2"]))
-end
-
 
 def test_isbn_10_with_x
 	assert_equal(true,check_number_isbn10(["978 5 471 48x"]))
@@ -40,36 +33,38 @@ def test_isbn_10_with_spaces
 	assert_equal(true,check_number_isbn10(["978 5 471 486", "978 5 471 489"]))
 end
 
+#Just check valid format ISBN(use def check_number_isbn10 before)
+def test_isbn_check_sum_10
+	assert_equal(true,check_sum_10(["0471958697","0-321-14653-0","877195869x"]))
+end
+
+def test_isbn_check_sum_10_invalid
+	assert_equal(false,check_sum_10(["0471958695","0-321-14653-1","877195868x"]))
+end
+
+
+#ISBN 13 TESTS
+def test_isbn_13_length
+	assert_equal(true,check_length(["978 5 471 486-425"]))
+end
+
+def test_isbn_13_incorret_number_of_chars
+	assert_equal(false,check_length(["978 471 486-1-1-2"]))
+end
+
 def test_isbn_has_13_with_invalid_chars
 	assert_equal(false,check_number_isbn13(["142536473625 1","978-0 471-888aaa"]))
 end
 
+def test_isbn_check_sum_13
+	assert_equal(true,check_sum_13(["9780470059029", "978-0-13-149505-0", "978 0 471 48648 0"]))
 end
 
-# def test_isbn_special_char
-#  	assert_equal(false,check_is_number(["970()471-48"]))
-# end
+def test_isbn_check_sum_13_invalid
+	assert_equal(false,check_sum_13(["9780470059029", "978-0-13-149505-0", "978 0 471 48648 3"]))
+end
 
-# def test_isbn_with_last_X
-#  	assert_equal(true,check_last_char_x(["978 5 471 48X"]))
-# end
 
-# def test_isbn_with_last_not_X
-#  	assert_equal(false,check_last_char_x(["978 5 471 48C"]))
-# end
-
-# def test_isbn_10_check_digit
-# 	assert_equal(true,check_isbn_10(["7421394761","877195869x"]))
-# end
-
-# def test_isbn_10_check_digit_wrong_num
-# 	assert_equal(false,check_isbn_10(["7421394766"]))
-# end	
-
-# def test_isbn_13_check_digit
-# 	assert_equal(true,check_isbn_13(["9780470059029"]))
-# end
-
-# end
+end
 
 
