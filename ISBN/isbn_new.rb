c0f3e8ac1a79157df4aa9a157f4c453ae1b63fd8
doverh <doverheld@gmail.com>
2017-02-08 15:09:30 -0500

    Updating isbn for web

---
+++ ISBN/isbn_new.rb
@@ -0,0 +1,101 @@
+#Function to check if number has length allowed to ISBN10 and ISBN13
+def sanitize_isbn(isbn)
+	isbn_n = isbn.delete(' -')# alternative could be gsub(/[-\s+]/, "") 
+	return isbn_n
+end	
+
+def check_length(isbn)	
+		number = sanitize_isbn(isbn) 
+			 if (number.length == 10)	
+				check_number_isbn10(isbn)
+			 elsif (number.length ==13)
+			 	check_number_isbn13(isbn)
+			 else
+			 	return false
+			 end
+end
+
+
+def check_number_isbn10(isbn)
+#Function receive an array of numbers and check if they are ISBN    
+
+	#isbn.each do |isbn_n|
+    	#Delete spaces and hifens
+    	number = isbn.delete(' -') #Alternative could be .gsub(/[-\s+]/, "") 
+    	#Separate last digit to check if is X or x
+    		isbn_number = number[0..8]
+    		isbn_digit = number[9]
+
+    		if (isbn_number.count("^0-9").zero? && isbn_digit.count("^0-9xX").zero?)
+    			return check_sum_10(isbn)
+    		else
+    			return false
+    		end
+	
+end
+
+
+def check_sum_10(isbn)
+
+	#isbn.each do |isbn_number|
+		total = 0
+		position = 1
+		isbn[0..8].each_char do |calc_num|
+			total += calc_num.to_i * position
+			position +=1
+		end
+		total = total % 11
+		puts "Total ISBN 10 = #{total}" 
+		if ((isbn[9] == 'X' || isbn[9] == 'x')  && total == 10)
+			return true
+		elsif (isbn[9].to_i == total)
+			return true
+		else
+			return false
+		end
+	#end	
+	
+end
+
+	
+def check_number_isbn13(isbn)
+#Function receive an array of numbers and check if they are ISBN    
+
+	#isbn.each do |isbn_n|
+    	#Delete spaces and hifens
+    	number = isbn.delete(' -')
+    	#Separate last digit to check if is X or x
+    		if number.count("^0-9").zero?
+    			return check_sum_13(isbn)
+    		else
+    			return false
+    		end
+end
+
+
+def check_sum_13(isbn)
+
+	#isbn.each do |isbn_number|
+		total = 0
+		position = 1
+		isbn[0..11].each_char do |calc_num|
+			position.even? ? total += calc_num.to_i * 3 : total += calc_num.to_i * 1
+			#puts "position: #{position}"
+			#puts "total: #{total}"
+						position +=1
+					end
+			
+		total = (10 - (total % 10)) % 10
+		#puts "Total = #{total}"
+		#puts "last = #{isbn_number[12]}"
+		
+		if (total == isbn[12].to_i)
+			return true		
+		else 
+			return false
+		end	
+	#end
+	
+end
+
+
---
+++ ISBN/test_isbn_new.rb
@@ -0,0 +1,73 @@
+# Requirements
+# - Takes a string
+# - Check for invalid chars
+# - Accept X on last position
+# - Return true or false
+# - Check position of string 
+
+require "minitest/autorun"
+require_relative "isbn_new.rb"
+
+class TestIsbn < Minitest::Test
+
+
+#ISBN10 TESTS ALL TESTS SHOULD CALL SAME FUNCTION
+
+def test_isbn_lenght_10_chars
+	assert_equal(true,check_length("978 5 471 486"))
+end
+
+def test_isbn_incorret_number_of_chars
+	assert_equal(false,check_length("9784862111"))
+end
+
+def test_isbn_check_sum_13
+	assert_equal(true,check_length("9780470059029"))
+end
+
+def test_isbn_check_sum_13_invalid
+	assert_equal(false,check_length("9780470059028"))
+end
+
+end
+# def test_isbn_10_with_x
+# 	assert_equal(true,check_number_isbn10(["978 5 471 48x"]))
+# end
+
+# def test_isbn_10_has_invalid_chars
+# 	assert_equal(false,check_number_isbn10(["978-0-471-aaa"]))
+# end
+
+# def test_isbn_10_with_spaces
+# 	assert_equal(true,check_number_isbn10(["978 5 471 486", "978 5 471 489"]))
+# end
+
+# #Just check valid format ISBN(use def check_number_isbn10 before)
+# def test_isbn_check_sum_10
+# 	assert_equal(true,check_sum_10(["0471958697","0-321-14653-0","877195869x"]))
+# end
+
+# def test_isbn_check_sum_10_invalid
+# 	assert_equal(false,check_sum_10(["0471958695","0-321-14653-1","877195868x"]))
+# end
+
+
+# #ISBN 13 TESTS
+# def test_isbn_13_length
+# 	assert_equal(true,check_length(["978 5 471 486-425"]))
+# end
+
+# def test_isbn_13_incorret_number_of_chars
+# 	assert_equal(false,check_length(["978 471 486-1-1-2"]))
+# end
+
+# def test_isbn_has_13_with_invalid_chars
+# 	assert_equal(false,check_number_isbn13(["142536473625 1","978-0 471-888aaa"]))
+# end
+
+
+
+
+
+
+
